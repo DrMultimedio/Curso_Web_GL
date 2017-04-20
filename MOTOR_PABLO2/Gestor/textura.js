c9112f69a901b5bc3cde2ce03bc54e9efe2d7111
Jose Manuel <coveromaki@gmail.com>
2017-04-20 15:56:23 +0200

    Texturas 0.3

--- MOTOR_PABLO2/Gestor/textura.js
+++ MOTOR_PABLO2/Gestor/textura.js
@@ -20,6 +20,46 @@ Textura.prototype.setImagen = function(file){
 Textura.prototype.handleLoadedTexture = function() {
 
 
+		cubeVerticesTextureCoordBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesTextureCoordBuffer);
+		  
+		var textureCoordinates = [
+		  // Front
+		  0.0,  0.0,
+		  1.0,  0.0,
+		  1.0,  1.0,
+		  0.0,  1.0,
+		  // Back
+		  0.0,  0.0,
+		  1.0,  0.0,
+		  1.0,  1.0,
+		  0.0,  1.0,
+		  // Top
+		  0.0,  0.0,
+		  1.0,  0.0,
+		  1.0,  1.0,
+		  0.0,  1.0,
+		  // Bottom
+		  0.0,  0.0,
+		  1.0,  0.0,
+		  1.0,  1.0,
+		  0.0,  1.0,
+		  // Right
+		  0.0,  0.0,
+		  1.0,  0.0,
+		  1.0,  1.0,
+		  0.0,  1.0,
+		  // Left
+		  0.0,  0.0,
+		  1.0,  0.0,
+		  1.0,  1.0,
+		  0.0,  1.0
+		];
+
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates),
+		              gl.STATIC_DRAW);
+
+
 		console.info('loading image');
 		gl.bindTexture(gl.TEXTURE_2D, this.textura);
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.imagen);
@@ -29,6 +69,7 @@ Textura.prototype.handleLoadedTexture = function() {
 	    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
 	    gl.generateMipmap(gl.TEXTURE_2D);
+
 		gl.bindTexture(gl.TEXTURE_2D, null);
 
 		console.log(this);
