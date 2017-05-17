f41ccf18d5849bbf3d38597b356d88ad5bb3aac6
Jose Manuel <coveromaki@gmail.com>
2017-05-17 07:41:05 +0200

    Revert "frustado estoy"
    This reverts commit 4468081f117491d01803481af177bb1a4421fe35.

--- MOTOR/Fachada/Motor.js
+++ MOTOR/Fachada/Motor.js
@@ -184,7 +184,6 @@ Motor.prototype.getLucesActivasDif = function() {
 };
 Motor.prototype.drawInitProgram = function() {
     gl = Utils.getGLContext("canvas-element-id");
-	gl = WebGLDebugUtils.makeDebugContext(gl, undefined, logGLCall);
 
 	var useVertexColors = false;
 	var texture = null;
--- MOTOR/Gestor/textura.js
+++ MOTOR/Gestor/textura.js
@@ -22,14 +22,29 @@ Textura.prototype.setCoordinates = function(coord) {
 	this.handleLoadedTexture();
 };
 Textura.prototype.handleLoadedTexture = function() {
-	
-	console.info('Cargando tex');
-	gl.bindTexture(gl.TEXTURE_2D, this.textura);
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.imagen);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-    gl.generateMipmap(gl.TEXTURE_2D);
-	gl.bindTexture(gl.TEXTURE_2D, null);
+
+
+		cubeVerticesTextureCoordBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesTextureCoordBuffer);
+		  
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.textureCoordinates),
+		              gl.STATIC_DRAW);
+
+
+		console.info('loading image');
+		gl.bindTexture(gl.TEXTURE_2D, this.textura);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.imagen);
+	    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+	    gl.generateMipmap(gl.TEXTURE_2D);
+
+		gl.bindTexture(gl.TEXTURE_2D, null);
+
+		console.log(this);
+
 
 
 };
